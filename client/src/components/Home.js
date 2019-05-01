6330ebabb4866d86c8cd8f10b6675eb9ee72d038
Yuko <yuko.mazurek@gmail.com>
2019-05-01 12:45:49 -0700

    add cycle and date length in list

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -13,19 +13,101 @@ class Home extends React.Component {
     today = dateFns.format(today, 'x');
 
     const index = dataFromServer.findIndex(obj => obj.dateIDms >= today);
+
     for (let i = index; i > 0; i--) {
       if (dataFromServer[i].period.start) {
-        console.log(dataFromServer[i]);
+        console.log(dataFromServer[i].dateIDms);
         let dayday = today - dataFromServer[i].dateIDms;
+
         const a = dayday / 86400000;
-        console.log(a);
+
         return a;
       }
     }
   };
+  showLength = () => {
+    const dataFromServer = JSON.parse(localStorage.getItem('data'));
+    if (!dataFromServer) return '';
+
+    const pAll = dataFromServer.filter(obj => obj.period);
+    const arr = [];
+    pAll.map((item, index) => {
+      if (item.period.start && pAll[index + 1].period.end) {
+        arr.push((pAll[index + 1].dateIDms - item.dateIDms) / 86400000);
+      } else {
+        if (item.period.start && pAll[index + 1].period.start) {
+          arr.push('?');
+        }
+      }
+    });
+    console.log('arr', arr);
+
+    return arr.map(item => <p>for -{item}-days</p>);
+    // const pS = dataFromServer.filter(obj => obj.period.start);
+
+    // const startIndexs = pS.map(item => pAll.findIndex(obj => obj === item));
+    // console.log(startIndexs);
+
+    // const eIndexs = [];
+
+    // startIndexs.map(index => {
+    //   for (let i = index; i < dataFromServer.length; i++) {
+    //     console.log(i);
+    //     if (dataFromServer[i].period.end) {
+    //       eIndexs.push(i);
+    //     }
+    //   }
+    // });
+
+    // console.log(eIndexs);
+  };
+  showDays = () => {
+    const dataFromServer = JSON.parse(localStorage.getItem('data'));
+    if (!dataFromServer) return '';
+
+    const arr = [];
+
+    const a = dataFromServer.filter(obj => obj.period.start);
+    console.log(a);
+
+    //もしendが記入されてなかった場合は「？」にしたい
+
+    const b = a.map((item, index) => {
+      if (a[index + 1]) {
+        return (a[index + 1].dateIDms - item.dateIDms) / 86400000;
+      } else {
+        return '';
+      }
+    });
+    console.log(b);
+
+    return b.map(item => <p>{item}</p>);
+  };
   showList = () => {
     const dataFromServer = JSON.parse(localStorage.getItem('data'));
     if (!dataFromServer) return '';
+
+    const arr = [];
+
+    const a = dataFromServer.filter(obj => obj.period.start);
+    console.log(a);
+
+    //もしendが記入されてなかった場合は「？」にしたい
+
+    const b = a.map((item, index) => {
+      if (a[index + 1]) {
+        return (a[index + 1].dateIDms - item.dateIDms) / 86400000;
+      } else {
+        return '?';
+      }
+    });
+    console.log(b);
+
+    return a.map(item => (
+      <p>
+        {item.month}-{item.day}
+      </p>
+    ));
 
     // const arr = [];
     // const arr2 = [];
@@ -60,6 +142,8 @@ class Home extends React.Component {
       <div>
         <p>{this.sinceHowManyDay()}...DAYS SINCE LAST PERIOD</p>
         {this.showList()}
+        {this.showDays()}
+        {this.showLength()}
       </div>
     );
   }
